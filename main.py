75c1e2b342e7d04b7a97af080e4e970ca681f920
joaquin1401 <joaquindesza@gmail.com>
2025-06-07 20:25:33 -0300

    PROBANDO EL DEPLOY

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hola")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hola, esta es una app desplegada con Render y CI"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000) # El port=10000 es requerido por Render para apps en Python.
