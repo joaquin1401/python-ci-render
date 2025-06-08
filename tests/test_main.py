dd3bc698e3dd6ae20d65efe3ed1c2a42798b416d
joaquin1401 <joaquindesza@gmail.com>
2025-06-07 21:08:14 -0300

    test_sobre_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,10 @@
 import unittest
+import home from main # type: ignore
 
 class TestBasico(unittest.TestCase):
     def test_dummy(self):
         self.assertEqual(2 + 2, 4)
+
+class TestPrueba(unittest.TestCase):
+    def test_dummy(self):
+        self.assertEqual(home.home(), "Hola, esta es una app desplegada con Render y CI")  
