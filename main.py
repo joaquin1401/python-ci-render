2be34b3e6067a4125e0b6e82f505fa97e2c5cdde
joaquin1401 <joaquindesza@gmail.com>
2025-06-12 11:56:05 -0300

    simulando el parcial

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola, esta es una app desplegada con Render y CI"
+    return "simulo que esty en el parcial"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000) # El port=10000 es requerido por Render para apps en Python.
